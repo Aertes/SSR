a4d671fb737fd4044b2c4bb69562d306fdb9b71c
Ares <mrxu1010@outlook.com>
2021-03-08 16:37:18 +0800

    nest-server

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { UserController } from './cats/user/user.controller';
 @Module({
   imports: [CatsModule],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
